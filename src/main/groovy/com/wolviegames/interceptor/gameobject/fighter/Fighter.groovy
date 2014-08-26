d32b2ac32bcbf5dc6b85e390edc21203bdab9a27
wolvie <wolvie@wolviegames.com>
2014-08-25 21:30:58 -0600

    Added Fighter class.

---
+++ src/main/groovy/com/wolviegames/interceptor/gameobject/fighter/Fighter.groovy
@@ -0,0 +1,27 @@
+package com.wolviegames.interceptor.gameobject.fighter
+
+import groovy.transform.ToString;
+
+@ToString
+class Fighter {
+	
+	// TODO figure out relationship between fighter and pilot/gunner
+	String faction // (enumeration laters?)
+	String name
+	Integer mass
+	Integer leftEngine
+	Integer rightEngine
+	Integer centerEngine
+	Boolean antigravity
+	Boolean streamlined
+	
+	// Shields
+	// Armor
+	// Weapons
+	
+	Integer velocity
+	Integer facing
+	Boolean seriouslyOutOfControl
+	Boolean destroyed
+	
+}
