ff930a63c3a929b9d9f03fa4a8b440ffe3adc332
wolvie <wolvie@wolviegames.com>
2015-02-28 15:43:14 -0700

    Added unit tests for Team.

---
+++ src/test/groovy/com/wolviegames/interceptor/game/TeamSpec.groovy
@@ -0,0 +1,35 @@
+package com.wolviegames.interceptor.game
+
+import com.wolviegames.interceptor.system.Coordinates
+import spock.lang.Specification
+
+class TeamSpec extends Specification{
+
+    def "Team will create a teamname and a faction"() {
+        when:
+        Team renegadeTeam = new Team("renegadeTeamName", Faction.RENEGADE)
+        Team togTeam = new Team("togTeamName", Faction.TOG)
+
+        then:
+        renegadeTeam.teamName == "renegadeTeamName"
+        renegadeTeam.faction == Faction.RENEGADE
+        togTeam.teamName == "togTeamName"
+        togTeam.faction == Faction.TOG
+    }
+
+    def "Team can add fighters to its team"() {
+        setup:
+        Team renegadeTeam = new Team("renegadeTeamName", Faction.RENEGADE)
+        Fighter fighterOne = new Fighter(new Coordinates(xCoord:0,yCoord:0))
+        Fighter fighterTwo = new Fighter(new Coordinates(xCoord:0,yCoord:1))
+
+        when:
+        renegadeTeam.addFighter(fighterOne)
+        renegadeTeam.addFighter(fighterTwo)
+
+        then:
+        renegadeTeam.getFighters().size() == 2
+        renegadeTeam.getFighters().contains(fighterOne)
+        renegadeTeam.getFighters().contains(fighterTwo)
+    }
+}
