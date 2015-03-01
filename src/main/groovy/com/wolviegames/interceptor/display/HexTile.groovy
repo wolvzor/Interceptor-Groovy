0f6f067e26df270a3b11a207be08a1d18a2a5fcf
wolvzor <wolvie@wolviegames.com>
2015-02-28 21:41:03 -0700

    Removed unused HexTile function

--- src/main/groovy/com/wolviegames/interceptor/display/HexTile.groovy
+++ src/main/groovy/com/wolviegames/interceptor/display/HexTile.groovy
@@ -12,10 +12,6 @@ class HexTile {
         image = ImageIO.read(getClass().getClassLoader().getResource("HexTile.png"));
     }
 
-    public void draw(Graphics graphics) {
-        graphics.drawImage(image, 30, 30, null);
-    }
-
     public void draw(Graphics graphics, int xCoord, int yCoord, double scale=1.0, DrawingOffset drawingOffset) {
         graphics.drawImage(image, xCoord + drawingOffset.width_offset, yCoord + drawingOffset.height_offset, (image.getWidth()*scale).intValue(), (image.getHeight()*scale).intValue(), null);
     }
